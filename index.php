6caa789ff1942009cc61effb994a41c46ad8f1ad
Peter <dhondt.peter@gmail.com>
2021-02-18 12:20:22 +0100

    added total revenue counter COOKIE

--- index.php
+++ index.php
@@ -26,9 +26,43 @@ $email = "";
 $valid_fields = 0;
 $confirmation_msg = "";
 $delivery_time = date("H:i:s", strtotime("+2 Hours"));
+$price = 0;
+
+if (isset($_COOKIE['totalValue'])) {
+    $totalValue = intval($_COOKIE['totalValue']);
+} else {
+    $totalValue = 0;
+    setcookie('totalValue', $totalValue, time() + (86400 * 30));
+}
+//
 
 // don't initialize empty session variables, otherwise the information is not contained
 // $_SESSION['streetName'] = $_SESSION['streetNumber'] = $_SESSION['city'] = $_SESSION['zipCode'] = "";
+
+//your products with their price.
+$products_food = [
+    ['name' => 'Club Ham', 'price' => 3.20],
+    ['name' => 'Club Cheese', 'price' => 3],
+    ['name' => 'Club Cheese & Ham', 'price' => 4],
+    ['name' => 'Club Chicken', 'price' => 4],
+    ['name' => 'Club Salmon', 'price' => 5]
+];
+
+$products_drinks = [
+    ['name' => 'Cola', 'price' => 2],
+    ['name' => 'Fanta', 'price' => 2],
+    ['name' => 'Sprite', 'price' => 2],
+    ['name' => 'Ice-tea', 'price' => 3],
+];
+
+// make the food items the default when loading the page
+if (!isset($_GET['food'])) {
+    $products = $products_food;
+} else if ($_GET['food'] == 0) {  // when order drinks is clicked, show the drinks items. Otherwise show the food items
+    $products = $products_drinks;
+} else {
+    $products = $products_food;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -73,50 +107,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['zipCode'] = "Please enter a valid Zipcode";
     }
 
+    foreach ($products AS $i => $product) {
+        if (isset($_POST['products'][$i])) {
+            $value = $_POST['products'][$i];
+            if ($value == 1) {
+                $price += $product['price'];
+            }
+        }
+    }
+
     if (isset($_POST['express_delivery'])) {
         $delivery_time = date("H:i:s", strtotime("+45 Minutes"));
+        $price += 5;
     }
+
+    //$totalValue = intval($_COOKIE['totalValue']);
+    $totalValue += $price;
+    $totalValue = strval($totalValue);
+    $_COOKIE['totalValue'] = $totalValue;
+    setcookie('totalValue', $totalValue, time() + (86400 * 30));
+
 
     if ($valid_fields == 5) {
-        $confirmation_msg = "Thank you. Your order has been sent. The delivery time is " . $delivery_time;
+        $confirmation_msg = "Thank you for your order. The delivery time is " . $delivery_time . ". The price is " . $price . "€.";
     }
 
 }
 
-//your products with their price.
-$products_food = [
-    ['name' => 'Club Ham', 'price' => 3.20],
-    ['name' => 'Club Cheese', 'price' => 3],
-    ['name' => 'Club Cheese & Ham', 'price' => 4],
-    ['name' => 'Club Chicken', 'price' => 4],
-    ['name' => 'Club Salmon', 'price' => 5]
-];
-
-$products_drinks = [
-    ['name' => 'Cola', 'price' => 2],
-    ['name' => 'Fanta', 'price' => 2],
-    ['name' => 'Sprite', 'price' => 2],
-    ['name' => 'Ice-tea', 'price' => 3],
-];
-
-// make the food items the default when loading the page
-if (!isset($_GET['food'])) {
-    $products = $products_food;
-} else if ($_GET['food'] == 0) {  // when order drinks is clicked, show the drinks items. Otherwise show the food items
-    $products = $products_drinks;
-} else {
-    $products = $products_food;
-}
-
-
-$totalValue = 0;
-
-    require 'form-view.php';
-
-
-
-
-
+require 'form-view.php';
 
 
 
